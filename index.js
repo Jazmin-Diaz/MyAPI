da1c2233e9eaec66f6bc7962c5372e7892b9e868
Jazmín-Díaz <natividadjazminp@gmail.com>
2024-04-15 14:03:15 -0600

    Mensaje descriptivo de los cambios realizados

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ import express from "express";
 import cors from "cors";
 import routes from "./routes";
 import mongoose from "mongoose";
-
+require("dotenv").config();
 
 
 const app = express();
+app.set("port", process.env.PORT || 4000);
 
 //conexion a base de datos
 mongoose.Promise=global.Promise;
-const dbUrl='mongodb://127.0.0.1:27017/informacion';
-mongoose.connect(dbUrl)
-.then(mongoose=>console.log('Conectado a la bd en el puerto 27017'));
-//Listening de puertos
-app.set('port', process.env.PORT || 4000);
+mongoose.connect(process.env.MONGODB_URI)
+.then(mongoose=>console.log('Conectado al MongoDB Atlas'));
 
-app.use(cors());
+
+//Listening de puertos
+
+app.set(cors());
 
 
 app.use(express.json());
@@ -27,4 +28,13 @@ app.use("/api",routes);
 app.listen(app.get('port'), () => {
   console.log(`Example app listening on port:` + app.get('port'));
 })
+
+
+
+
+
+
+
+
+
 
